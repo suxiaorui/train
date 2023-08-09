76d46740b27b5670866a2ea7356febed300b6d22
suxiaorui <aaa1097395576@foxmail.com>
2023-08-09 09:05:19 +0800

    修改启动日志

--- member/src/main/java/com/rui/train/member/config/MemberApplication.java
+++ member/src/main/java/com/rui/train/member/config/MemberApplication.java
@@ -1,5 +1,7 @@
 package com.rui.train.member.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,8 +10,11 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("com.rui")
 public class MemberApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(MemberApplication.class, args);
+        LOG.info("启动成功~");
     }
 
 }
