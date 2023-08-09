4e325257953cd7d11e3c769a285a6bd1eb090136
suxiaorui <aaa1097395576@foxmail.com>
2023-08-09 08:45:53 +0800

    新增member会员模块

---
+++ member/src/main/java/com/rui/train/member/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.rui.train.member.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author suxiaorui
+ * @Description TODO
+ * @Date 2023/8/9 8:15
+ * @Version 1.0
+ */
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello~~~";
+    }
+}
