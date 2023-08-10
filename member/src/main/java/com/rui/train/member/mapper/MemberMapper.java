2635eca47fd1d3a11d0ed478bd85983ec64aa0c0
suxiaorui <aaa1097395576@foxmail.com>
2023-08-10 19:48:45 +0800

    集成Mybatis官方代码生成器

--- member/src/main/java/com/rui/train/member/mapper/MemberMapper.java
+++ member/src/main/java/com/rui/train/member/mapper/MemberMapper.java
@@ -1,15 +1,32 @@
 package com.rui.train.member.mapper;
 
+import com.rui.train.member.domain.Member;
+import com.rui.train.member.domain.MemberExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
-/**
- * @Author suxiaorui
- * @Description TODO
- * @Date 2023/8/10 18:55
- * @Version 1.0
- */
 @Repository
 public interface MemberMapper {
+    long countByExample(MemberExample example);
 
-    int count();
+    int deleteByExample(MemberExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Member record);
+
+    int insertSelective(Member record);
+
+    List<Member> selectByExample(MemberExample example);
+
+    Member selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Member record, @Param("example") MemberExample example);
+
+    int updateByExample(@Param("record") Member record, @Param("example") MemberExample example);
+
+    int updateByPrimaryKeySelective(Member record);
+
+    int updateByPrimaryKey(Member record);
 }
--- member/src/main/java/com/rui/train/member/service/MemberService.java
+++ member/src/main/java/com/rui/train/member/service/MemberService.java
@@ -17,6 +17,6 @@ public class MemberService {
     private MemberMapper memberMapper;
 
     public int count(){
-        return memberMapper.count();
+        return Math.toIntExact(memberMapper.countByExample(null));
     }
 }
