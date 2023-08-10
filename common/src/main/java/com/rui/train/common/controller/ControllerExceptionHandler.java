09212e2a3163e8b6d6a300ce899bb7356ea19bb0
suxiaorui <aaa1097395576@foxmail.com>
2023-08-10 21:50:48 +0800

    增加统一的异常处理

---
+++ common/src/main/java/com/rui/train/common/controller/ControllerExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.rui.train.common.controller;
+
+/**
+ * @Author suxiaorui
+ * @Description TODO
+ * @Date 2023/8/10 21:45
+ * @Version 1.0
+ */
+
+import com.rui.train.common.resp.CommonResp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 统一异常处理、数据预处理等
+ */
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ControllerExceptionHandler.class);
+
+    /**
+     * 所有异常统一处理
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public CommonResp exceptionHandler(Exception e) throws Exception {
+        CommonResp commonResp = new CommonResp();
+        LOG.error("系统异常：", e);
+        commonResp.setSuccess(false);
+//        commonResp.setMessage("系统出现异常，请联系管理员");
+        commonResp.setMessage(e.getMessage());
+        return commonResp;
+    }
+}
