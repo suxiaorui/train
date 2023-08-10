b87ba6157c2275feb04f48869f9018f2957f749c
suxiaorui <aaa1097395576@foxmail.com>
2023-08-10 21:28:46 +0800

    测试一下注册接口

--- member/src/main/java/com/rui/train/member/service/MemberService.java
+++ member/src/main/java/com/rui/train/member/service/MemberService.java
@@ -1,8 +1,13 @@
 package com.rui.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.rui.train.member.domain.Member;
+import com.rui.train.member.domain.MemberExample;
 import com.rui.train.member.mapper.MemberMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @Author suxiaorui
@@ -18,5 +23,23 @@ public class MemberService {
 
     public int count(){
         return Math.toIntExact(memberMapper.countByExample(null));
+    }
+
+    public long register(String mobile){
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> list = memberMapper.selectByExample(memberExample);
+
+        if (CollUtil.isNotEmpty(list)){
+//            return list.get(0).getId();
+            throw new RuntimeException("手机号已注册");
+        }
+
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
+
     }
 }
