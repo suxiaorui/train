7431422cd4b814806e422d408a23be1327c0da47
suxiaorui <aaa1097395576@foxmail.com>
2023-08-12 18:16:30 +0800

    制作欢迎页面

---
+++ web/src/views/main/welcome.vue
@@ -0,0 +1,15 @@
+<template>
+    <p style="font-size: 20px; font-weight: bold">欢迎使用小瑞12306售票系统</p>
+</template>
+
+<script>
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    return {
+    };
+  },
+});
+</script>
