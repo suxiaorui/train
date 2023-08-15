20a12a9a107775bc3a940a5719a5635ef09d154f
suxiaorui <aaa1097395576@foxmail.com>
2023-08-15 18:38:57 +0800

    增加business业务模块

---
+++ business/src/main/java/com/rui/train/business/config/BusinessApplication.java
@@ -0,0 +1,21 @@
+package com.rui.train.business.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.rui")
+@MapperScan("com.rui.train.*.mapper")
+public class BusinessApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BusinessApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessApplication.class, args);
+        LOG.info("启动成功~");
+    }
+}
