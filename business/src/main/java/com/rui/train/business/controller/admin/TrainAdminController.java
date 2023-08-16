0159d8c674de9867e802375c04c18dace8c2dc0d
suxiaorui <aaa1097395576@foxmail.com>
2023-08-16 16:50:26 +0800

    火车管理的增删改查

---
+++ business/src/main/java/com/rui/train/business/controller/admin/TrainAdminController.java
@@ -0,0 +1,55 @@
+package com.rui.train.business.controller.admin;
+
+import com.rui.train.business.req.TrainQueryReq;
+import com.rui.train.business.req.TrainSaveReq;
+import com.rui.train.business.resp.TrainQueryResp;
+import com.rui.train.business.service.TrainService;
+import com.rui.train.common.resp.CommonResp;
+import com.rui.train.common.resp.PageResp;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @Author suxiaorui
+ * @Description TODO
+ * @Date 2023/8/16 16:06
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping("/admin/train")
+public class TrainAdminController {
+
+    @Resource
+    private TrainService trainService;
+
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody TrainSaveReq req) {
+        trainService.save(req);
+        return new CommonResp<>();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<TrainQueryResp>> queryList(@Valid TrainQueryReq req) {
+        PageResp<TrainQueryResp> list = trainService.queryList(req);
+        return new CommonResp<>(list);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp<Object> delete(@PathVariable Long id) {
+        trainService.delete(id);
+        return new CommonResp<>();
+    }
+
+    @GetMapping("/query-all")
+    public CommonResp<List<TrainQueryResp>> queryList() {
+        List<TrainQueryResp> list = trainService.queryAll();
+        return new CommonResp<>(list);
+    }
+
+
+}
+
---
+++ business/src/main/java/com/rui/train/business/req/TrainQueryReq.java
@@ -0,0 +1,19 @@
+package com.rui.train.business.req;
+
+import com.rui.train.common.req.PageReq;
+
+/**
+ * @Author suxiaorui
+ * @Description TODO
+ * @Date 2023/8/16 16:09
+ * @Version 1.0
+ */
+public class TrainQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "TrainQueryReq{" +
+                "} " + super.toString();
+    }
+}
+
