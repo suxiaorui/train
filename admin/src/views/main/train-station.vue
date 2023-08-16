278af25cc11118ba8f22ec4aa3ace7b04eeee30b
suxiaorui <aaa1097395576@foxmail.com>
2023-08-16 19:53:47 +0800

    前端增加车次选择下拉框

--- admin/src/views/main/train-station.vue
+++ admin/src/views/main/train-station.vue
@@ -28,7 +28,12 @@
            ok-text="确认" cancel-text="取消">
     <a-form :model="trainStation" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
       <a-form-item label="车次编号">
-        <a-input v-model:value="trainStation.trainCode"></a-input>
+        <a-select v-model:value="trainStation.trainCode" show-search
+                  :filterOption="filterTrainCodeOption">
+          <a-select-option v-for="item in trains" :key="item.code" :value="item.code" :label="item.code + item.start + item.end">
+            {{item.code}} | {{item.start}} ~ {{item.end}}
+          </a-select-option>
+        </a-select>
       </a-form-item>
       <a-form-item label="站序">
         <a-input v-model:value="trainStation.index" />
@@ -221,11 +226,36 @@ export default defineComponent({
       });
     };
 
+    //车次下拉框
+    const trains = ref([]);
+
+    //查询所有的车次，用于车次下拉框
+    const queryTrainCode = () => {
+      axios.get("/business/admin/train/query-all").then((response) => {
+        loading.value = false;
+        let data = response.data;
+        if (data.success) {
+          trains.value = data.content;
+        }else {
+          notification.error({description: data.message});
+        }
+      });
+    };
+
+    //车次下拉框筛选
+    const filterTrainCodeOption = (input, option) => {
+      console.log(input,option);
+      return option.label.toLowerCase().indexOf(input.toLowerCase()) >= 0;
+    }
+
+
     onMounted(() => {
       handleQuery({
         page: 1,
         size: pagination.value.pageSize
       });
+
+      queryTrainCode();
     });
 
     return {
@@ -241,7 +271,9 @@ export default defineComponent({
       handleOk,
       onEdit,
       onDelete,
-      params
+      params,
+      filterTrainCodeOption,
+      trains
     };
   },
 });
