eb041bb5232d3a7e52a724788b53b167bca657a9
suxiaorui <aaa1097395576@foxmail.com>
2023-08-18 20:20:55 +0800

    订票页面，优化购票列表展示，勾选乘客后提交再次显示进行确认

--- web/src/views/main/order.vue
+++ web/src/views/main/order.vue
@@ -19,10 +19,67 @@
   <a-divider></a-divider>
   <b>勾选要购票的乘客：</b>&nbsp;
   <a-checkbox-group v-model:value="passengerChecks" :options="passengerOptions" />
-  <br/>
-  选中的乘客：{{passengerChecks}}
-  <br/>
-  购票列表：{{tickets}}
+  <div class="order-tickets">
+    <a-row class="order-tickets-header" v-if="tickets.length > 0">
+      <a-col :span="2">乘客</a-col>
+      <a-col :span="6">身份证</a-col>
+      <a-col :span="4">票种</a-col>
+      <a-col :span="4">座位类型</a-col>
+    </a-row>
+    <a-row class="order-tickets-row" v-for="ticket in tickets" :key="ticket.passengerId">
+      <a-col :span="2">{{ticket.passengerName}}</a-col>
+      <a-col :span="6">{{ticket.passengerIdCard}}</a-col>
+      <a-col :span="4">
+        <a-select v-model:value="ticket.passengerType" style="width: 100%">
+          <a-select-option v-for="item in PASSENGER_TYPE_ARRAY" :key="item.code" :value="item.code">
+            {{item.desc}}
+          </a-select-option>
+        </a-select>
+      </a-col>
+      <a-col :span="4">
+        <a-select v-model:value="ticket.seatTypeCode" style="width: 100%">
+          <a-select-option v-for="item in seatTypes" :key="item.code" :value="item.code">
+            {{item.desc}}
+          </a-select-option>
+        </a-select>
+      </a-col>
+    </a-row>
+  </div>
+  <div v-if="tickets.length > 0">
+    <a-button type="primary" size="large" @click="finishCheckPassenger">提交订单</a-button>
+  </div>
+
+  <a-modal v-model:visible="visible" title="请核对以下信息"
+           style="top: 50px; width: 800px"
+           ok-text="确认" cancel-text="取消"
+           @ok="showFirstImageCodeModal">
+    <div class="order-tickets">
+      <a-row class="order-tickets-header" v-if="tickets.length > 0">
+        <a-col :span="3">乘客</a-col>
+        <a-col :span="15">身份证</a-col>
+        <a-col :span="3">票种</a-col>
+        <a-col :span="3">座位类型</a-col>
+      </a-row>
+      <a-row class="order-tickets-row" v-for="ticket in tickets" :key="ticket.passengerId">
+        <a-col :span="3">{{ticket.passengerName}}</a-col>
+        <a-col :span="15">{{ticket.passengerIdCard}}</a-col>
+        <a-col :span="3">
+          <span v-for="item in PASSENGER_TYPE_ARRAY" :key="item.code">
+            <span v-if="item.code === ticket.passengerType">
+              {{item.desc}}
+            </span>
+          </span>
+        </a-col>
+        <a-col :span="3">
+          <span v-for="item in seatTypes" :key="item.code">
+            <span v-if="item.code === ticket.seatTypeCode">
+              {{item.desc}}
+            </span>
+          </span>
+        </a-col>
+      </a-row>
+    </div>
+  </a-modal>
 </template>
 
 <script>
@@ -75,6 +132,8 @@ export default defineComponent({
     //   seat: "C1"
     // }
     const tickets = ref([]);
+    const PASSENGER_TYPE_ARRAY = window.PASSENGER_TYPE_ARRAY;
+    const visible = ref(false);
 
     // 勾选或去掉某个乘客时，在购票列表中加上或去掉一张表
     watch(() => passengerChecks.value, (newVal, oldVal)=>{
@@ -105,6 +164,18 @@ export default defineComponent({
       });
     };
 
+    const finishCheckPassenger = () => {
+      console.log("购票列表：", tickets.value);
+
+      if (tickets.value.length > 5) {
+        notification.error({description: '最多只能购买5张车票'});
+        return;
+      }
+
+      // 弹出确认界面
+      visible.value = true;
+    }
+
     onMounted(() => {
       handleQueryPassenger();
     })
@@ -115,7 +186,10 @@ export default defineComponent({
       seatTypes,
       passengerOptions,
       passengerChecks,
-      tickets
+      tickets,
+      PASSENGER_TYPE_ARRAY,
+      visible,
+      finishCheckPassenger
     };
   },
 });
@@ -126,9 +200,31 @@ export default defineComponent({
   font-size: 18px;
   font-weight: bold;
 }
+.order-train .order-train-ticket {
+  margin-top: 15px;
+}
 .order-train .order-train-ticket .order-train-ticket-main {
   color: red;
   font-size: 18px;
+}
+.order-tickets {
+  margin: 10px 0;
+}
+.order-tickets .ant-col {
+  padding: 5px 10px;
+}
+.order-tickets .order-tickets-header {
+  background-color: cornflowerblue;
+  border: solid 1px cornflowerblue;
+  color: white;
+  font-size: 16px;
+  padding: 5px 0;
+}
+.order-tickets .order-tickets-row {
+  border: solid 1px cornflowerblue;
+  border-top: none;
+  vertical-align: middle;
+  line-height: 30px;
 }
 </style>
 
