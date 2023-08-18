adcf494d16a83debe27cb321eaa4e64f78a529b0
suxiaorui <aaa1097395576@foxmail.com>
2023-08-18 09:54:31 +0800

    增加预定按钮，点击预定时，跳转到下单页面，并用sessionStorage传递参数

---
+++ web/src/views/main/order.vue
@@ -0,0 +1,21 @@
+<template>
+   <div>{{ dailyTrainTicket }}</div>
+</template>
+
+<script>
+
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  name: "order-view",
+  setup() {
+    const dailyTrainTicket = SessionStorage.get("dailyTrainTicket") || {};
+    console.log("下单的车次信息", dailyTrainTicket);
+
+    return {
+      dailyTrainTicket
+    };
+  },
+});
+</script>
+
