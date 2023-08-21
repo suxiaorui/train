87db9652a136643676f65592b0ca06fec51f4009
suxiaorui <aaa1097395576@foxmail.com>
2023-08-21 23:48:49 +0800

    使用Sentinal组件进行限流降级

--- business/src/main/java/com/rui/train/business/config/BusinessApplication.java
+++ business/src/main/java/com/rui/train/business/config/BusinessApplication.java
@@ -1,5 +1,8 @@
 package com.rui.train.business.config;
 
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,6 +10,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @ComponentScan("com.rui")
@@ -19,5 +25,21 @@ public class BusinessApplication {
     public static void main(String[] args) {
         SpringApplication.run(BusinessApplication.class, args);
         LOG.info("启动成功~");
+
+        //  限流规则
+        initFlowRules();
+        LOG.info("已定义限流规则");
+    }
+
+
+    private static void initFlowRules(){
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule rule = new FlowRule();
+        rule.setResource("doConfirm");
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        // Set limit QPS to 20.
+        rule.setCount(1);
+        rules.add(rule);
+        FlowRuleManager.loadRules(rules);
     }
 }
